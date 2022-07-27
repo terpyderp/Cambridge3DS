fbed48dca6ae7f306db35ad74d2ca4ac1e3af41b
TERPYDERP32 <44077453+terpyderp@users.noreply.github.com>
2022-07-27 12:38:46 -0500

    gaming

--- build/game/load/bigint.lua
+++
@@ -1,2 +0,0 @@
-bigint = require "libs.bigint.bigint"
-number_names = require "libs.bigint.named-powers-of-ten"
--- build/game/tetris/randomizers/always.lua
+++
@@ -1,9 +0,0 @@
-local Randomizer = require 'tetris.randomizers.randomizer'
-
-local AlwaysRandomizer = Randomizer:extend()
-
-function AlwaysRandomizer:generatePiece()
-	return "I"
-end
-
-return AlwaysRandomizer
